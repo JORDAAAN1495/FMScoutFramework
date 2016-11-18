01e22e12bf4729d39359b2103b534c1ee8cdfd1e
Thanos Siopoudis <thanoulas@gmail.com>
2016-11-18 10:12:26 +0000

    Add extensions

---
+++ Extensions/EnumBindingSourceExtension.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Windows.Markup;
+
+namespace FMScoutFramework.Extensions
+{
+    public class EnumBindingSourceExtension : MarkupExtension
+    {
+        private Type _enumType;
+        public Type EnumType
+        {
+            get { return this._enumType; }
+            set
+            {
+                if (value != this._enumType)
+                {
+                    if (null != value)
+                    {
+                        Type enumType = Nullable.GetUnderlyingType(value) ?? value;
+
+                        if (!enumType.IsEnum)
+                        {
+                            throw new ArgumentException("Type must be for an Enum.");
+                        }
+
+                        this._enumType = value;
+                    }
+                }
+            }
+        }
+
+        public EnumBindingSourceExtension() { }
+        public EnumBindingSourceExtension(Type enumType)
+        {
+            this.EnumType = enumType;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return Enum.GetValues(EnumType)
+                .Cast<object>()
+                .Select(e => new { Value = (int)e, DisplayName = WeakEnumDescription(e) });
+        }
+
+        private string WeakEnumDescription(object e)
+        {
+            FieldInfo fi = e.GetType().GetField(e.ToString());
+            if (fi != null)
+            {
+                var attributes = (DescriptionAttribute[])fi.GetCustomAttributes(typeof(DescriptionAttribute), false);
+                return ((attributes.Length > 0) && (!String.IsNullOrEmpty(attributes[0].Description))) ? attributes[0].Description : e.ToString();
+            }
+
+            return "Unknown";
+        }
+    }
+}
---
+++ Extensions/FMSFExtensions.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace FMScoutFramework.Extensions
+{
+    public static class FMSFExtensions
+    {
+        public static string GetDescription<T>(this T value)
+        {
+            Type type = typeof(T);
+            string name = Enum.GetName(type, value);
+            if (name != null)
+            {
+                FieldInfo field = type.GetField(name);
+                if (field != null)
+                {
+                    DescriptionAttribute attr = Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute)) as DescriptionAttribute;
+                    if (attr != null)
+                    {
+                        return attr.Description;
+                    }
+                }
+            }
+
+            return value.ToString();
+        }
+
+        public static int ConvertToInt(this object value)
+        {
+            return (int)value;
+        }
+
+        public static string GetDescription(this Enum value)
+        {
+            Type type = value.GetType();
+            string name = Enum.GetName(type, value);
+            if (name != null)
+            {
+                FieldInfo field = type.GetField(name);
+                if (field != null)
+                {
+                    DescriptionAttribute attr = Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute)) as DescriptionAttribute;
+                    if (attr != null)
+                    {
+                        return attr.Description;
+                    }
+                }
+            }
+
+            return value.ToString();
+        }
+
+        public static IEnumerable<T> GetEnumValues<T>()
+        {
+            return Enum.GetValues(typeof(T)).Cast<T>();
+        }
+
+        public static T ToEnum<T>(this string value)
+        {
+            return (T)Enum.Parse(typeof(T), value, true);
+        }
+    }
+}
