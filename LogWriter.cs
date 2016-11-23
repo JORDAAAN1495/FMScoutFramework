c84a076e02754296064f70c09fa7a86444274fc4
Thanos Siopoudis <thanoulas@gmail.com>
2016-11-23 15:46:37 +0000

    Add a log writer class

---
+++ LogWriter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace FMScoutFramework {
+    public class LogWriter {
+        private string m_exePath = string.Empty;
+        public LogWriter(string logMessage) {
+            LogWrite(logMessage);
+        }
+        public void LogWrite(string logMessage) {
+            m_exePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            try {
+                using (StreamWriter w = File.AppendText(m_exePath + "\\" + "log.txt")) {
+                    Log(logMessage, w);
+                }
+            }
+            catch (Exception ex) {
+            }
+        }
+
+        public void Log(string logMessage, TextWriter txtWriter) {
+            try {
+                txtWriter.Write("{0} {1}", DateTime.Now.ToLongTimeString(),
+                    DateTime.Now.ToLongDateString());
+                txtWriter.WriteLine(":{0}", logMessage);
+            }
+            catch (Exception ex) {
+            }
+        }
+    }
+}
