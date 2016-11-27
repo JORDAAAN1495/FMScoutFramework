160ec525ee12c557ec060bd7e761871c5d7d602a
Thanos Siopoudis <thanoulas@gmail.com>
2016-11-27 21:44:33 +0000

    Update Club and City offsets for FM17

--- Defines/Offsets/CityOffsets.cs
+++ Defines/Offsets/CityOffsets.cs
@@ -13,11 +13,14 @@ namespace FMScoutFramework.Defines.Offsets
 
         public const short RowID = 0x4;
         public const short ID = 0x8;
-        public const int Nation = 0x18;
-        public const short Name = 0x10;
-        public const short Attraction = 0x38;
-        public const short Latitude = 0x24;
-        public const short Longitude = 0x28;
-        public const short Altitude = 0x34;
+        public const short Name = 0x18;
+        public const short NationPtr = 0x20;
+        public const short LanguagePtr = 0x28;
+        public const short Latitude = 0x48;
+        public const short Longitude = 0x4C;
+        public const short LocalRegionPtr = 0x2C;
+        public const short Altitude = 0x50;
+        public const short InhabitantsRange = 0x52;
+        public const short Attraction = 0x54;
     }
 }
--- Defines/Offsets/ClubOffsets.cs
+++ Defines/Offsets/ClubOffsets.cs
@@ -13,45 +13,45 @@ namespace FMScoutFramework.Core.Offsets
         }
 
         // Consts are the same for every version
-        public const short RowID = 0x4;
-        public const short ID = 0x8;
-        public const short Teams = 0x10;
-        public const short ClubDetailsOne = 0x50;
+        public const short RowID = 0x8;
+        public const short UID = 0xC;
+        public const short Teams = 0x18;
+        public const short ClubInfoOne = 0x98;
 
         public short Fullname {
-            get { return 0x64; }
+            get { return 0xA0; }
         }
 
         public short Name {
-            get { return 0x68; }
+            get { return 0xA8; }
         }
 
         public short ShortName {
-            get { return 0x6C; }
+            get { return 0xB0; }
         }
 
         public short Nation {
-            get { return 0x74; }
+            get { return 0xB8; }
         }
 
         public short BasedNation {
-            get { return 0x7C; }
+            get { return 0xD0; }
         }
 
         public short City {
-            get { return 0x80; }
+            get { return 0xD8; }
         }
 
-        public short ClubDetailsTwo {
-            get { return 0x70; }
+        public short ClubInfoTwo {
+            get { return 0xE0; }
         }
 
         public short ClubSponshorshipDeals {
-            get { return 0x88; }
+            get { return 0x110; }
         }
 
         public short ClubFinances {
-            get { return 0xB0; }
+            get { return 0x130; }
         }
     }
 }
