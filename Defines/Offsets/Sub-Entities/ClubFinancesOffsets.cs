f290ca2ae63e23fb621d3123c4d7cfd68004e389
Thanos Siopoudis <thanoulas@gmail.com>
2016-11-27 21:44:51 +0000

    Update ClubFinancesOffsets for FM17

--- Defines/Offsets/Sub-Entities/ClubFinancesOffsets.cs
+++ Defines/Offsets/Sub-Entities/ClubFinancesOffsets.cs
@@ -12,34 +12,35 @@ namespace FMScoutFramework.Core.Offsets
             this.Version = version;
         }
 
-        public const short Balance = 0xC;
-        public const short AverageTicketPrice = 0x10;
-        public const short AverageSeasonTicketPrice = 0x14;
-        public const short EmbargoStartDate = 0x24;
-        public const short EmbargoEndDate = 0x28;
-        public const short EmbargoAppealDate = 0x2C;
-        public const short SugarDaddy = 0x31;
-        public const short RemainingBudget = 0x3C;
-        public const short SeasonTransferFunds = 0x40;
-        public const short TransferIncomePercentage = 0x44;
-        public const short YouthGrantIncome = 0x4C;
-        public const short HighestWagePaid = 0x84;
-
-        public short LatestSeasonTicketSales {
-            get { return 0x0; }
-        }
-
-        public short WeeklyWageBudget {
-            get { return 0x70; }
-        }
-
-        public short HighestWage {
-            get { return 0x74; }
-        }
-
-        public short WeeklyWageBudgetUsed {
-            get { return 0x78; }
-        }
-
+        public const short Balance = 0x18;
+        public const short AverageTicketPrice = 0x1C;
+        public const short AverageSeasonTicketPrice = 0x20;
+        public const short MatchTicketPriceRatio = 0x24;
+        public const short SeasonTicketPriceRatio = 0x28;
+        public const short RatioForChangeInSeasonTicketHolders = 0x2C;
+        public const short EmbargoStartDate = 0x30;
+        public const short EmbargoEndDate = 0x34;
+        public const short EmbargoAppealDate = 0x38;
+        public const short SugarDaddy = 0x3D;
+        public const short RemainingBudget = 0x48;
+        public const short SeasonTransferFunds = 0x4C;
+        public const short TransferIncomePercentage = 0x50;
+        public const short YouthGrantIncome = 0x58;
+        public const short StadiumRentalPerYear = 0x74;
+        public const short StartingLastYearsTurnover = 0x78;
+        public const short WeeklyWageBudget = 0x80;
+        public const short HighestWage = 0x84;
+        public const short WeeklyWageBudgetUsed = 0x8C;
+        public const short HighestWagePaid = 0xA0;
+        public const short HighestNonPlayerWagePaid = 0xA4;
+        public const short LatestSeasonTicketSales = 0xD0;
+        public const short FFPMaxWeeklyWageTotal = 0xC4;
+        public const short EnteredFSBankruptState = 0x498;
+        public const short TrainingExpansionFlag = 0x4A6;
+        public const short YouthExpansionFlag = 0x4A7;
+        public const short StateOfEmergency = 0x4AE;
+        public const short StadiumRentalPercentageOfGateReceipts = 0x4B1;
+        public const short CorporateFacilitiesRevenueLevel = 0x4B2;
+        public const short IncreateStaffWages = 0x4B3;
     }
 }
