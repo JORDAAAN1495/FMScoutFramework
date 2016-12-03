73d427277ca929bb51b59767c039af1856b92e50
Thanos Siopoudis <thanoulas@gmail.com>
2016-12-03 18:09:08 +0000

    Remove unneeded imports

--- Defines/Offsets/Sub-Entities/ClubDebtsOffsets.cs
+++ Defines/Offsets/Sub-Entities/ClubDebtsOffsets.cs
@@ -1,9 +1,4 @@
 ﻿using FMScoutFramework.Core.Entities.GameVersions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FMScoutFramework.Core.Offsets {
     public sealed class ClubDebtsOffsets {
