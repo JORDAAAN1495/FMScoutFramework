336acb4ef772cea697064995e414352c420fcf00
Thanos Siopoudis <thanoulas@gmail.com>
2016-12-03 18:08:43 +0000

    Empty the Finances interface until we finalize it

--- Entities/Ingame/Interfaces/IClubFinances.cs
+++ Entities/Ingame/Interfaces/IClubFinances.cs
@@ -2,15 +2,5 @@
 {
     public interface IClubFinances
     {
-        int Balance { get; set; }
-        int HighestWagePaid { get; set; }
-        int LastestSeasonTickets { get; set; }
-        int LatestSeasonTicketsAddress { get; set; }
-        int RemainingBudget { get; set; }
-        int SeasonTransferFunds { get; set; }
-        int TransferIncomePercentage { get; set; }
-        int WeeklyWageBudget { get; set; }
-        int HighestWage { get; set; }
-        int YouthGrantIncome { get; set; }
     }
 }
