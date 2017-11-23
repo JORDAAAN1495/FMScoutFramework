482422c0a65b547dba9532a63a3eb3cb217df9ca
Thanos Siopoudis <siopoudis@me.com>
2017-11-23 20:50:57 +0000

    Disable LogWriter for mac as it tampers with the signed bundle

--- LogWriter.cs
+++ LogWriter.cs
@@ -2,24 +2,39 @@
 using System.IO;
 using System.Reflection;
 
-namespace FMScoutFramework {
-    public class LogWriter {
+namespace FMScoutFramework
+{
+    public class LogWriter
+    {
         private string m_exePath = string.Empty;
-        public LogWriter(string logMessage) {
+        public LogWriter(string logMessage)
+        {
             LogWrite(logMessage);
         }
-        public void LogWrite(string logMessage) {
+        public void LogWrite(string logMessage)
+        {
+#if MAC
+            return;
+#else
             m_exePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-            try {
-                using (StreamWriter w = File.AppendText(m_exePath + "\\" + "log.txt")) {
+            try
+            {
+                using (StreamWriter w = File.AppendText(m_exePath + "\\" + "log.txt"))
+                {
                     Log(logMessage, w);
                 }
             }
-            catch (Exception ex) {
+            catch (Exception ex)
+            {
             }
+#endif
         }
 
-        public void Log(string logMessage, TextWriter txtWriter) {
+        public void Log(string logMessage, TextWriter txtWriter)
+        {
+#if MAC
+            return;
+#else
             try {
                 txtWriter.Write("{0} {1}", DateTime.Now.ToLongTimeString(),
                     DateTime.Now.ToLongDateString());
@@ -27,6 +42,7 @@ namespace FMScoutFramework {
             }
             catch (Exception ex) {
             }
+#endif
         }
     }
 }
