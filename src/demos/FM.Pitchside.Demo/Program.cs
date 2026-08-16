e4f4bf4a5041347a3a0221a50f2f2ee757fa03f6
JORDAAAN1495 <jordaaan1495@gmail.com>
2026-08-16 16:41:33 +0100

    Add demo

---
+++ src/demos/FM.Pitchside.Demo/Program.cs
@@ -0,0 +1,15 @@
+﻿using FMScoutFramework.Core;
+
+var core = new FMCore();
+
+core.LoadData();
+
+var players = core.Players
+    .Where(x => x.ActualPerson.LastName.Equals("Rashford", StringComparison.OrdinalIgnoreCase))
+    .ToList();
+
+foreach (var player in players)
+{
+    Console.WriteLine(player.ActualPerson.VisibleName);
+    Console.WriteLine(player.Team.Name);
+}
